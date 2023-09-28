e5008ba299607d8b30e23362a69a1a2b94f119e3
venkat vegi <vv952057@student.nitw.ac.in>
2023-09-28 11:51:55 +0530

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,58 @@
+import React, { useEffect, createContext, useReducer, useContext } from "react";
+import NavBar from "./components/Navbar";
+import "./App.css";
+import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom"; // Updated import statements
+import Home from "./components/screens/Home";
+import Signin from "./components/screens/SignIn";
+import Profile from "./components/screens/Profile";
+import Signup from "./components/screens/Signup";
+import CreatePost from "./components/screens/CreatePost";
+import { reducer, initialState } from "./reducers/userReducer";
+import UserProfile from "./components/screens/UserProfile";
+import SubscribedUserPosts from "./components/screens/SubscribesUserPosts";
+
+export const UserContext = createContext();
+
+const Routing = () => {
+  const navigate = useNavigate();
+  const { state, dispatch } = useContext(UserContext);
+
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("user"));
+    if (user) {
+      dispatch({ type: "USER", payload: user });
+    } else {
+      navigate("/signin");
+    }
+  }, []);
+
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/signin" element={<Signin />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/create" element={<CreatePost />} />
+      <Route path="/profile/:userid" element={<UserProfile />} />
+      <Route path="/myfollowingpost" element={<SubscribedUserPosts />} />
+      {/* <Route path="/reset" element={<Reset />} />
+      <Route path="/reset/:token" element={<NewPassword />} /> */}
+      {/* Add a catch-all route for unknown paths */}
+      {/* <Route path="*" element={<Navigate to="/" />} /> */}
+    </Routes>
+  );
+};
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  return (
+    <UserContext.Provider value={{ state, dispatch }}>
+      <BrowserRouter>
+        <NavBar />
+        <Routing />
+      </BrowserRouter>
+    </UserContext.Provider>
+  );
+}
+
+export default App;
